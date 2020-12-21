c56fecce42706ba0a3498aedb4fb62d573188b67
Alina <alinasoldatkina2014@gmail.com>
2020-12-21 11:37:05 +0300

    Добавлен скрипт для формы "Автомобиль""

---
+++ ptest_auto.js
@@ -0,0 +1,44 @@
+﻿// namespace
+var Navicon = Navicon || {};
+
+Navicon.ptest_auto = (function () {
+
+    // Событие на изменение в поле Тип
+    var usedOnChange = function (context) {
+
+        let formContext = context.getFormContext();
+
+        let usedControl = formContext.getControl("ptest_used");
+        let kmControl = formContext.getControl("ptest_km");
+        let ownerscountControl = formContext.getControl("ptest_ownerscount");
+        let isdamagedControl = formContext.getControl("ptest_isdamaged");
+
+        kmControl.setVisible(usedControl.getValue() == "Да");
+        ownerscountControl.setVisible(usedControl.getValue() == "Да");
+        isdamagedControl.setVisible(usedControl.getValue() == "Да");
+
+    }
+
+    return {
+        onLoad: function (context) {
+
+            let formContext = context.getFormContext();
+
+            let usedAttr = formContext.getAttribute("ptest_used");
+            let kmControl = formContext.getControl("ptest_km");
+            let ownerscountControl = formContext.getControl("ptest_ownerscount");
+            let isdamagedControl = formContext.getControl("ptest_isdamaged");
+
+            if (usedAttr == null || kmControl == null || ownerscountControl == null || isdamagedControl == null) return;
+
+            console.log(formContext.getControl("ptest_used").getName());
+            console.log(formContext.getControl("ptest_used").getControlType());
+            console.log(formContext.getControl("ptest_used").getValue());
+            console.log(formContext.getAttribute("ptest_used").getInitialValue());
+
+            usedAttr.addOnChange(usedOnChange);
+
+        }
+    }
+})();
+
