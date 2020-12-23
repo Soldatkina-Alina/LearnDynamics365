2a240d33981b74b1f88842eee79238c95b4636fb
Alina <alinasoldatkina2014@gmail.com>
2020-12-23 21:47:05 +0300

    Добавлено ограничение для модели для сист. админа.

---
+++ ptest_model.js
@@ -0,0 +1,20 @@
+﻿// namespace
+var Navicon = Navicon || {};
+
+Navicon.ptest_model = (function () {
+
+    return {
+        onLoad: function (context) {
+
+            let formContext = context.getFormContext();
+
+            for (let role of Xrm.Utility.getGlobalContext().userSettings.roles.get())
+                if (role.name == "Системный администратор")
+                    for (let sec of formContext.ui.tabs.get("tabs_1").sections.get())
+                        sec.controls.get().forEach(function (item, i, arr) {
+                            item.setDisabled(true);
+                        });
+        }
+    }
+})();
+
